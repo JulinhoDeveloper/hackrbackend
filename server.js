653c5565225889682dcc6ad42428b3e728491294
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-09-25 12:02:15 -0300

    configurando as rotas e controllers de usuários

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// importar rotas
+const authRouter = require('./routes/auth');
 
+//middlewares
+app.use('/api/', authRouter);
 
 const port = process.env.PORT || 8000;
 
