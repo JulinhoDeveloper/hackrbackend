ac16a7263099d26a2c347adf039970774d5e2b6a
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-09-25 14:53:09 -0300

    instalações

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const morgan = require('morgan');
+const bodyParser  = require('body-parser');
+const cors  = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 // importar rotas
 const authRouter = require('./routes/auth');
+const { json } = require('body-parser');
+
+//app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors()); 
 
 //middlewares
 app.use('/api/', authRouter);
